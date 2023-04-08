e5291de14b87a6393b300d48e2509b8dd918f330
Leapz1 <90947456+Leapz1@users.noreply.github.com>
2023-04-08 13:29:11 +0800

    main menu

---
+++ Assets/dip.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dip : MonoBehaviour
+{
+    public void ExitButton(){
+        Application.Quit();
+        Debug.Log("u dipped");
+    }
+}
